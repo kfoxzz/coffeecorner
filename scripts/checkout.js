12d41102094423b9de5bf9cafdd32105c8da4852
Kristi Zentner <kristi.zentner17@gmail.com>
2021-09-14 00:11:40 +0200

    Added checkout modal

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 let itemArray = [];
 let keys = Object.keys(localStorage);
+
 for (let key of keys) {
         const quantity = localStorage.getItem(key);
         itemArray.push({
@@ -8,7 +9,7 @@ for (let key of keys) {
         });
 }
 
-window.onload = function() {
+const updateCart = () => {
         for (i = 0; i < itemArray.length; i++) {
                 const priceId = itemArray[i].price;
                 if (document.getElementById(priceId)) {
@@ -17,6 +18,41 @@ window.onload = function() {
                         itemElem.querySelector('.quantity').innerHTML = `x ${itemArray[i].quantity}`
                 }
         }
+        if (itemArray.length === 0) {
+                document.getElementById("cartItems").innerHTML = "Your cart is empty";
+        }
+}
+
+window.onload = updateCart();
+
+const removeFromCart = (priceId) => {
+        const itemElem = document.getElementById(priceId);
+        itemElem.style.display = "none";
+        localStorage.removeItem(priceId);
+        for (i = 0; i < itemArray.length; i++) {
+                const itemPrice = itemArray[i].price;
+                if (itemPrice === priceId) {
+                        itemArray.splice(i, 1);
+                }
+        }
+        if (itemArray.length === 0) {
+                document.getElementById("cartItems").innerHTML = "Your cart is empty";
+        }
+}
+
+const addToCart = (quantitySelected, priceId) => {
+        const quantity = document.getElementById(quantitySelected).value;
+        if (quantity === "Quantity") {
+                alert('Select a quantity to add to cart.');
+        } else {
+                if (window.localStorage.getItem(priceId)) {
+                        const newQuantity = parseInt(quantity) + parseInt(window.localStorage.getItem(priceId))
+                        window.localStorage.setItem(priceId, newQuantity);
+                } else {
+                        window.localStorage.setItem(priceId, quantity);
+                }
+        }
+        location.reload();
 }
 
 const initiateCheckout = () => {
@@ -35,7 +71,5 @@ const initiateCheckout = () => {
         .then(response => response.json())
         .then(data => window.location = data.url);
 }
-
-
 
 
